1edb3bb92c17edb7c8b6e21d9abaee9169616c7a
jerik244 <juliobeserra0@gmail.com>
2025-01-21 20:37:37 -0300

    subindo direto do intellij - faz o L

---
+++ src/main/java/com/triSynch/estoqueAlmoxarifado/entity/usuarioEntity.java
@@ -0,0 +1,85 @@
+package com.triSynch.estoqueAlmoxarifado.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "eA_USUARIO")
+public class usuarioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String nome_de_usuario;
+
+    @Column(nullable = false)
+    private String setor;
+
+    @Column(nullable = false)
+    private String cargo;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senhaHash;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome_de_usuario() {
+        return nome_de_usuario;
+    }
+
+    public void setNome_de_usuario(String nome_de_usuario) {
+        this.nome_de_usuario = nome_de_usuario;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
+}
