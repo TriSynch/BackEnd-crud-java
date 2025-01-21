14e99c9be07e78eb80594f160a2efb6244456301
Julio Barros <juliobeserra0@gmail.com>
2025-01-21 19:26:32 -0300

    Add files via upload
    recomecei do zero, vou fazer os updates via intellij

---
+++ estoqueAlmoxarifado/src/main/java/com/triSynch/estoqueAlmoxarifado/EstoqueAlmoxarifadoApplication.java
@@ -0,0 +1,13 @@
+package com.triSynch.estoqueAlmoxarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueAlmoxarifadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueAlmoxarifadoApplication.class, args);
+	}
+
+}
---
+++ estoqueAlmoxarifado/src/test/java/com/triSynch/estoqueAlmoxarifado/EstoqueAlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triSynch.estoqueAlmoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueAlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
